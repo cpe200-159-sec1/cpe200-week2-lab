5c386a868a9e55a765c93fc4794e80a137b9c475
satiando5826 <sathianpornkhamdee@gmail.com>
2016-08-30 14:05:21 +0700

    580610686  lab2_2  finish !

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -21,6 +21,8 @@ public class ArrayStack {
     public void push(Object o)
     {
         /* Your code here */
+        storage[top + 1] = o;
+        top++;
     }
 
     public Object pop()
@@ -33,7 +35,7 @@ public class ArrayStack {
     public int size()
     {
         /* Your code here */
-        return 0;
+        return top+1;
     }
 
     public static void main(String[] args) {
