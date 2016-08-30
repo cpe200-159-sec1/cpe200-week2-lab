aecdd4845168eb444dce320be5022b77b0614840
thanaphom <pamdhumawong@gmail.com>
2016-08-30 14:11:14 +0700

    580610644 lab_week_2

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,24 @@
  * Implement grading system.
  */
 public class GPA {
+    float G,Grade,SumG;
+    int C,SumC;
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+        C=credit;
+        G=grade;
+        SumG+=G*C;
+        SumC+=C;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+        Grade=SumG/SumC;
+        if(SumC==0){
+            return 0;
+        }
+        else {
+            return Grade;
+        }
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
