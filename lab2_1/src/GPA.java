fe32b4cfcbf1d88a27e85cf384727ab70569780b
khimmyparty@gmail.com <KhimKhim12>
2016-08-30 16:10:06 +0700

    580610625

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,13 +2,28 @@
  * Implement grading system.
  */
 public class GPA {
+
+    public float GPA = 0;
+    public float creditAndGrade = 0;
+    public float sumCredit = 0 ;
+
+
     public void addGrade(int credit, float grade) {
         /* Your code here */
+        sumCredit+=credit ;
+        creditAndGrade += (credit*grade) ;
     }
 
 
     public float get() {
         /* Your code here */
+        GPA = creditAndGrade/sumCredit ;
+
+        if (GPA==0.0 || GPA<=4.0)
+        {
+
+            return GPA;
+        }
         return 0.0f;
     }
     public static void main(String[] args) {
